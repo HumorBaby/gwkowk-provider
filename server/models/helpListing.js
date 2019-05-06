e5600ac2b4dbf621d0b3ae1c4289cb0002349c44
Humorous Baby <humorbaby@humorbaby.net>
2019-05-05 23:47:58 -0400

    server: add HelpListing model schema and basic tests

---
+++ server/models/helpListing.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose')
+
+let conn
+const defineSchema = config => {
+  const conn = require('../services/database')(config)
+
+  const helpListingSchema = new mongoose.Schema({
+    /**
+    * Help listing document schema.
+    */
+
+    // TODO: validations; e.g., empty/require fields
+
+    listingId: { // alphanum char string to ID listing; generated on save
+      type: String,
+      index: true,
+      unique: true
+    },
+    botNick: String,
+    serverHostname: String,
+    helpPrefix: String,
+    // Help entries
+    modules: [{
+      _id: false,
+      moduleName: String,
+      entries: [{
+        _id: false,
+        commands: { type: [String] }, // Command(s)
+        doc: String, // Command doc/purpose
+        examples: { type: [String] } // Command example(s)
+      }]
+    }]
+  })
+
+  const CHARACTER_SPACE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  helpListingSchema.statics.genRandomId = function (length = 4) {
+    let result = []
+    while (result.length < length) {
+      result.push(CHARACTER_SPACE.charAt(Math.floor(Math.random() * CHARACTER_SPACE.length)))
+    }
+
+    return result.join('')
+  }
+
+  // avoid listingIds that are confusing if present in the API route path (e.g., help);
+  // undesired words can be added here as well (e.g., profanity).
+  // use lowercase _only_ for easier case-insensitive search; i.e., HeLP == hELp == help
+  const FORBIDDEN_STRINGS = ['help']
+  helpListingSchema.pre('save', async function (cb) {
+    let genIdRetriesRemaining = 10 // Number of times to try to get a valid/unique listingId
+    let newListingId
+
+    while (genIdRetriesRemaining-- > 0) {
+      newListingId = this.constructor.genRandomId()
+
+      if (
+        !(await this.constructor.findOne({ listingId: newListingId })) && // unique
+        !(FORBIDDEN_STRINGS.includes(newListingId.toLowerCase())) // not a forbidden string
+      ) {
+        this.listingId = newListingId
+        cb()
+        break
+      }
+    }
+    // Was not able to generate unique listing ID
+    cb(Error('Could not generate unique listing ID.'))
+  })
+
+  return conn.model('HelpListing', helpListingSchema)
+}
+
+let instance
+module.exports = config => {
+  if (instance && conn.models.HelpListing) { return instance }
+  instance = defineSchema(config)
+  return instance
+}
