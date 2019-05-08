00e5923d6eb9242f984885ee1f658f1746a087d7
Humorous Baby <humorbaby@humorbaby.net>
2019-05-08 17:34:47 -0400

    frontend: add non-functional search bar in menu

--- frontend/src/components/TheListingMenu.vue
+++ frontend/src/components/TheListingMenu.vue
@@ -1,6 +1,14 @@
 <template>
   <div v-if="listing">
     <ul class="menu-list">
+      <li class="menu-label">
+          <p class="control has-icons-left">
+            <input class="input is-small" type="text" placeholder="search">
+            <span class="icon is-small is-left">
+              <i class="fas fa-search" aria-hidden="true"></i>
+            </span>
+          </p>
+        </li>
       <template v-for="module in listing.modules">
         <li :key="module.moduleName">
           <a :href="'#'+module.moduleName+'-module'" class="menu-label">{{module.moduleName}}</a>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,12 @@
 const path = require('path')
 
 module.exports = {
-    devServer: {
-        proxy: {
-            '^/api': {
-                target: process.env.API_PROXY_URL,
-                changeOrigin: true
-            }
-        }
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: process.env.API_PROXY_URL,
+        changeOrigin: true
+      }
     }
   },
   pluginOptions: {
